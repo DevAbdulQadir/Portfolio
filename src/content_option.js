6e425939f8834d4efeb636c7bac93c73eb3a0545
Abdul Qadir <iaq.ags@gmail.com>
2025-03-06 20:43:48 +0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "AQ";
+const meta = {
+    title: "Abdul Qadir",
+    description: "I’m Abdul Qadir Game Developer currenlty working in Pakistan",
+};
+
+const introdata = {
+    title: "I’m Abdul Qadir",
+    animated: {
+        first: "I love coding",
+        second: "I enjoy bringing virtual worlds to life through code.",
+        third: "Love turning ideas into games.",
+        forth: "Nothing beats seeing players enjoy my work!"
+    },
+    description: "I am Computer System Engineer. I am working as Game Developer with 9+ years experience in Simulation, Action, Open World, Puzzle and Hyper Casual Games.",
+   // your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "https://i.postimg.cc/BbCbdfT2/IMG-23455.jpg",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "Hi, I'm AQ, a passionate game developer who loves turning ideas into immersive gaming experiences. With expertise in Unity C#, C++. I specialize in game design, programming, AI development, Shaders, Animations, optimization.",
+};
+
+
+const skills = [{
+        name: "Game Development",
+        value: 90,
+    },
+    {
+        name: "C++ / C#",
+        value: 85,
+    },
+    {
+        name: "Photoshop",
+        value: 80,
+    },
+    {
+        name: "Blender",
+        value: 60,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Game Development",
+        description: "i can code for 2d and 3d all genre games for web, android and iOS. i have 100+ games live on playstore",
+    },
+    {
+        title: "UI & UX Design",
+        description: "i can provide simple hyper casual project UI & UX Design",
+    },
+    {
+        title: "3d Modeler",
+        description: "i can optimize, retexture and modify 3d models",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/s2DSCzNx/Icon22.jpg",
+        description: "This Assassin Sniper Shooter is an exciting and realistic sniper game with a massive campaign mode.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.assassinsnipershooter",
+    },
+    {
+        img: "https://i.postimg.cc/DwQdKBfj/Battle-Royal.png",
+        description: "The best single player Battle royale offline game with strategic play, amazing graphics and diverse joyful experiences",
+        link: "https://play.google.com/store/apps/details?id=com.battleroyale.offlinegames.fortbattle.crazygames",
+    },
+    
+    {
+        img: "https://i.postimg.cc/4yh2vV6D/Laser.jpg",
+        description: " Experiment with the laser box mechanisms, reflectors, and barriers to the adorable fish trapped within each puzzle in fish rescue.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.LaserLogicPuzzle",
+    },
+    {
+        img: "https://i.postimg.cc/8c1sMmg5/Biker.jpg",
+        description: "Give up your engine and push the limits in heart-pounding races, experience the excitement of becoming a racing master. ",
+        link: "https://play.google.com/store/apps/details?id=com.AvariGameStudio.ExtremeSportsBikeRider",
+    },
+    {
+        img: "https://i.postimg.cc/4NP26fVN/Trpl-Match.jpg",
+        description: "Car matching game whose goal is to pair up identical vehicles amidst various other items. ",
+        link: "https://play.google.com/store/apps/details?id=com.ags.TripleMatchCarMaster",
+    },
+    {
+        img: "https://i.postimg.cc/HLhtSjkd/Gangstr.png",
+        description: "It’s a unique blend of mafia & gangster open world games.",
+        link: "https://play.google.com/store/apps/details?id=com.cb.gangster.crime.game",
+    },
+    {
+        img: "https://i.postimg.cc/c435rpBX/Car1.jpg",
+        description: "Dive into highly detailed environments and test everything you’ve learned about driving car simulator and parking.",
+        link: "https://play.google.com/store/apps/details?id=com.AvariGameStudio.DriveCarSimulator",
+    },
+    {
+        img: "https://i.postimg.cc/xC7td5kw/Fruit-Merge-Icon.jpg",
+        description: "Drop Merge game the central theme revolves around combining identical fruits through the innovative drop and merge mechanism.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.FruitMergeCrush2048",
+    },
+    {
+        img: "https://i.postimg.cc/NMykZXNp/Icon-Store.jpg",
+        description: "Hit the enemy who's color is matching with knife. Kill all enemies to complete the level. Just tap the right knife.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.KnifeJam.tap.away",
+    },
+    {
+        img: "https://i.postimg.cc/sDn1GLfG/Decore-Icon.png",
+        description: "Players must strategically merge objects to maximize space and points.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.decor.room.merge",
+    },
+    {
+        img: "https://i.postimg.cc/jqBnMmYV/Heli.png",
+        description: "Hold the flight stick of your helicopter to fly high.",
+        link: "https://play.google.com/store/apps/details?id=com.BestFreeGames.HelicopterSimulatorRescueGame",
+    },
+    {
+        img: "https://i.postimg.cc/vm8vGkmq/FlyCar.jpg",
+        description: "Now race on sky roads and be careful while flying through tall city buildings and skyscrapers.",
+        link: "https://play.google.com/store/apps/details?id=com.BestfreeGames.FlyingCarRealDriver",
+    },
+    {
+        img: "https://i.postimg.cc/jj6BFGFM/Airplane.jpg",
+        description: "Play airplane games and become a pilot by doing simulated flight in airplane simulator.",
+        link: "https://play.google.com/store/apps/details?id=com.mizo.plane.landing",
+    },
+    {
+        img: "https://i.postimg.cc/JzNTrj5n/AirRobot.jpg",
+        description: "Rise as a fly bot in an era of robot transformation to fight the future robot battles!",
+        link: "https://play.google.com/store/apps/details?id=com.mizo.air.robot.transform",
+    },
+    
+    {
+        img: "https://i.postimg.cc/ydt5gsgY/Fall.png",
+        description: "Tap at right time to avoid girl to fall.",
+        link: "https://play.google.com/store/apps/details?id=com.ags.FallGirl",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aqalive7997@gmail.com",
+    YOUR_FONE: "+92 300 7997903",
+    description: "I’m always excited to connect with fellow developers, designers, and gaming enthusiasts. Whether you have a project idea, want to collaborate, or just want to chat about game development, feel free to reach out!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_gb3lzrm",
+    YOUR_TEMPLATE_ID: "template_70otk4d",
+    YOUR_USER_ID: "6BjZ_pnfp0jeFGfGz",
+};
+
+const socialprofils = {
+    github: "https://github.com/DevAbdulQadir",
+    facebook: "https://facebook.com/amaas19",
+    linkedin: "https://www.linkedin.com/in/engr-abdul-qadir/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
